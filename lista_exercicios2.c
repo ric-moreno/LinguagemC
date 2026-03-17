e2c3b7e55e56eeb76f1d87e946df5f8bf027f68f
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-03-17 19:04:53 -0400

    Create lista_exercicios2.c

---
+++ lista_exercicios2.c
@@ -0,0 +1,60 @@
+/*1) Escreva um programa em linguagem C que imprima a seguinte mensagem na tela: “Eu vou
+aprender a programar em Linguagem C na disciplina Algoritmos e Programação I”.*/
+
+
+
+
+2) Altere o programa anterior para que a mensagem seja dividida e apresentada em três linhas
+diferentes.
+
+
+
+  
+3) Escreva um programa em linguagem C que leia 4 números inteiros, calcule a média aritmética
+entre esses números e imprima o resultado na tela. 
+
+
+
+  
+4) Escreva um programa em linguagem C que leia um número e imprima o seu antecessor e o seu
+sucessor.
+
+
+
+  
+5) Escreva um programa em linguagem C que leia uma temperatura na escala Celsius, converta para
+o seu valor correspondente nas escalas Fahrenheit e Kelvin e imprima esses valores na tela.
+
+
+  
+6) Escreva um programa em linguagem C que leia a base e a altura de um triângulo, calcule a área
+desse triângulo e imprima o resultado na tela.
+
+
+
+  
+7) Escreva um programa em linguagem C que leia o valor de uma velocidade em quilômetros por
+hora, calcule o valor correspondente em metros por segundo e imprima o resultado na tela.
+
+
+
+  
+8) Escreva um programa em linguagem C que leia o raio de uma circunferência, calcule a área dessa
+circunferência e imprima o resultado na tela.
+
+
+
+  
+9) Escreva um programa em linguagem C que leia um número real, calcule o triplo deste número e
+imprima o resultado na tela.
+
+
+  
+10) Escreva um programa em linguagem C que leia um número no formato CDU (centena, dezena e
+unidade) e imprima esse número invertido (UDC). Exemplo: quando o número lido for 123, o
+programa deve imprimir 321. Antes de imprimir o número invertido, ele deve ser armazenado em
+outra variável, diferente daquela usada para armazenar o número digitado. Dica: utilize o operador
+de resto da divisão (%).
+
+
+

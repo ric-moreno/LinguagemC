a5a6e461cee08370666eceb82edb1dc888d74bcd
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-03-17 20:34:58 -0400

    Update lista_exercicios2.c

--- lista_exercicios2.c
+++ lista_exercicios2.c
@@ -1,5 +1,4 @@
-/*1) Escreva um programa em linguagem C que imprima a seguinte mensagem na tela: “Eu vou
-aprender a programar em Linguagem C na disciplina Algoritmos e Programação I”.*/
+/*1) Escreva um programa em linguagem C que imprima a seguinte mensagem na tela: “Eu vou aprender a programar em Linguagem C na disciplina Algoritmos e Programação I”.*/
 
 #include<stdio.h>
 #include<locale.h>
@@ -18,8 +17,7 @@ int main()
 
 
 
-/*2) Altere o programa anterior para que a mensagem seja dividida e apresentada em três linhas
-diferentes.*/
+/*2) Altere o programa anterior para que a mensagem seja dividida e apresentada em três linhas diferentes.*/
 
 #include<stdio.h>
 #include<locale.h>
@@ -37,9 +35,9 @@ int main()
 
 
 
-  
-/*3) Escreva um programa em linguagem C que leia 4 números inteiros, calcule a média aritmética
-entre esses números e imprima o resultado na tela.*/
+
+
+/*3) Escreva um programa em linguagem C que leia 4 números inteiros, calcule a média aritmética entre esses números e imprima o resultado na tela.*/
 
 #include<stdio.h>
 #include<locale.h>
@@ -76,8 +74,7 @@ int main()
 
 
 
-/*4) Escreva um programa em linguagem C que leia um número e imprima o seu antecessor e o seu
-sucessor.*/
+/*4) Escreva um programa em linguagem C que leia um número e imprima o seu antecessor e o seu sucessor.*/
 
 #include<stdio.h>
 #include<locale.h>
@@ -103,9 +100,9 @@ int main()
 
 
 
-  
-/*5) Escreva um programa em linguagem C que leia uma temperatura na escala Celsius, converta para
-o seu valor correspondente nas escalas Fahrenheit e Kelvin e imprima esses valores na tela.*/
+
+
+/*5) Escreva um programa em linguagem C que leia uma temperatura na escala Celsius, converta para o seu valor correspondente nas escalas Fahrenheit e Kelvin e imprima esses valores na tela.*/
 
   #include<stdio.h>
 #include<locale.h>
@@ -131,11 +128,8 @@ int main()
 
 
 
-
-
   
-/*6) Escreva um programa em linguagem C que leia a base e a altura de um triângulo, calcule a área
-desse triângulo e imprima o resultado na tela.*/
+/*6) Escreva um programa em linguagem C que leia a base e a altura de um triângulo, calcule a área desse triângulo e imprima o resultado na tela.*/
 
 #include<stdio.h>
 #include<locale.h>
@@ -165,8 +159,7 @@ int main()
 
 
   
-/*7) Escreva um programa em linguagem C que leia o valor de uma velocidade em quilômetros por
-hora, calcule o valor correspondente em metros por segundo e imprima o resultado na tela.*/
+/*7) Escreva um programa em linguagem C que leia o valor de uma velocidade em quilômetros por hora, calcule o valor correspondente em metros por segundo e imprima o resultado na tela.*/
 
 #include<stdio.h>
 #include<locale.h>
@@ -247,10 +240,8 @@ int main()
 
 
   
-/*10) Escreva um programa em linguagem C que leia um número no formato CDU (centena, dezena e
-unidade) e imprima esse número invertido (UDC). Exemplo: quando o número lido for 123, o
-programa deve imprimir 321. Antes de imprimir o número invertido, ele deve ser armazenado em
-outra variável, diferente daquela usada para armazenar o número digitado. Dica: utilize o operador
+/*10) Escreva um programa em linguagem C que leia um número no formato CDU (centena, dezena e unidade) e imprima esse número invertido (UDC). Exemplo: quando o número lido for 123, 
+o programa deve imprimir 321. Antes de imprimir o número invertido, ele deve ser armazenado em outra variável, diferente daquela usada para armazenar o número digitado. Dica: utilize o operador
 de resto da divisão (%).*/
 
 #include<stdio.h>

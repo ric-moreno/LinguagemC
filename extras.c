ee4585d2b01f89a5e14dfed8070e80d094ed9f4d
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-03-25 15:24:28 -0400

    Update extras.c

--- extras.c
+++ extras.c
@@ -287,3 +287,36 @@ int main(){
 	
 	return 0;
 }
+
+
+
+
+
+/*7) Usando o comando switch, escreva um programa que leia um inteiro entre 1 e 7 e imprima o dia da semana correspondente a esse número. 
+Isto é, domingo, se 1, segunda-feira, se 2, e assim por diante.*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "portuguese");
+	
+	int dia;
+	
+	printf("Digite o dia da semana no formato (1-7): ");
+	scanf("%d", &dia);
+	
+	switch(dia){
+	
+		case 1: printf("Domingo\n"); break;
+		case 2: printf("Segunda-feira\n"); break;
+		case 3: printf("Terça-feira\n"); break;
+		case 4: printf("Quarta-feira\n"); break;
+		case 5: printf("Quinta-feira\n"); break;
+		case 6: printf("Sexta-feira\n"); break;
+		case 7: printf("Sábado\n"); break;
+	}
+	
+	return 0;
+}

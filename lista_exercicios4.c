a52a6131898c4878a1487980d71174b7e3245b0a
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-01 09:25:03 -0400

    Create lista_exercicios4.c

---
+++ lista_exercicios4.c
@@ -0,0 +1,73 @@
+/*1) Escreva um programa que imprima todos os múltiplos de 7, menores que 1000.*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int a = 7;
+	
+	while(a < 1000){
+		
+		printf("%d ", a);
+		a = a + 7;
+	}
+}
+
+
+
+
+/*2) Escreva um programa que calcule e imprima a soma de N números digitados. 
+Caso o usuário digite zero, o programa deve parar a leitura de números e imprimir o resultado. 
+O zero digitado não deve ser considerado no cálculo, embora essa soma não altere o resultado final.*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int num, soma = 0;
+	
+	while(num != 0){
+		
+		printf("Digite um numero ou 0 para interromper: ");
+		scanf("%d", &num);
+		
+		if (num != 0);
+		soma = soma + num;
+		
+	}
+	
+	printf("Soma: %d\n", soma);
+	return 0;
+}
+
+
+
+
+/*3) Escreva um programa que calcule e imprima a média de N números digitados. 
+Caso o usuário digite zero, o programa deve parar a leitura de números e imprimir o resultado. 
+O zero digitado não deve ser considerado no cálculo.*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int contador = 0;
+	float num, soma = 0, media;
+	
+	while(num != 0){
+		
+		printf("Digite um numero ou 0 para interromper: ");
+		scanf("%f", &num);
+		
+		if (num != 0){
+		
+		soma = soma + num;
+		contador++;
+				
+		}
+	}
+	
+	media = soma / contador;
+	printf("Media %.2f\n", media);
+	return 0;
+}

4834dcff9501825fd52c943437166fe3d90f0880
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-01 17:11:04 -0400

    Update lista_exercicios4.c

--- lista_exercicios4.c
+++ lista_exercicios4.c
@@ -223,4 +223,70 @@ int main(){
 
 
 
+/*9) Escreva um programa que leia dois números inteiros positivos (X e Y) diferentes e 
+imprima todos os números inteiros (pares e ímpares) que estão no intervalo formado pelos números digitados. 
+O programa deve testar qual dos números digitados é o menor e qual é o maior, antes de imprimir os números do intervalo.*/
 
+#include<stdio.h>
+
+int main(){
+	
+	int X, Y, inicio, fim;
+	
+	do{
+		
+		printf("Digite um numero inteiro positivo X: ");
+		scanf("%d", &X);
+		
+		printf("Digite um numero inteiro positivo Y: ");
+		scanf("%d", &Y);
+		
+		if(X < 0 || Y < 0 || X == Y)
+			printf("Digite um numero valido, positivo e diferente para X e Y\n");
+		
+	} while(X < 0 || Y < 0 || X == Y);
+	
+	if(X < Y){
+	
+		inicio = X;
+		fim = Y;
+	}
+	
+	else{
+	
+		inicio = Y;
+		fim = X;
+	}
+	
+	while(inicio < fim){
+		printf("%d ", inicio);
+		inicio++;
+	}
+	
+	return 0;
+	
+}
+
+
+
+
+/*10) Escreva um programa que imprima todos os múltiplos de 9 que estão no intervalo entre 100 e 1000.*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int a = 100;
+	
+	while(a < 1000){
+		
+		if(a % 9 == 0){
+		
+			printf("%d ", a);
+		}
+		
+		a++;
+	}
+	
+	return 0;
+}

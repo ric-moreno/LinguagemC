650511d92e51132011a8c106751290c62d81412f
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-02 14:00:49 -0400

    Update lista_exercicios5.c

--- lista_exercicios5.c
+++ lista_exercicios5.c
@@ -10,10 +10,40 @@ int main(){
 	printf("Digite um numero de 1 a 9: ");
 	scanf("%d", &num);
 	
-	for(i = 1; i <= 10; i++){
+	for(i = 1; i <= 9; i++){
 		
 		resultado = num * i;
 		printf("%d x %d = %d\n", num, i, resultado);
+		
+	}
+	
+	return 0;
+}
+
+
+
+
+/*2) Escreva um programa que imprima a tabuada completa dos números de 1 até 9, 
+considerando as quatro operações aritméticas básicas (soma, subtração, multiplicação e divisão).*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int num, i, soma, subt, mult;
+	float div;
+	
+	printf("Insira um numero de 1 a 9: ");
+	scanf("%d", &num);
+	
+	for(i = 1; i <=9; i++){
+		
+		soma = num + i;
+		subt = num - i;
+		mult = num * i;
+		div = num / i;
+		
+		printf("%d + %d = %d\t%d - %d = %d\t%d * %d = %d\t%d / %d = %.1f\n", i, num, soma, i, num, subt, i, num, mult, i, num, div);
 		
 	}
 	

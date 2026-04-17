db83909dd4aac7908d1b61820e60c73af22a8dce
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-17 12:06:40 -0400

    Create questao3.c

---
+++ simulado2/questao3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    int num1 = 1, num2 = 1, soma, i;
+    
+    for(i = 1; i <= 30; i++) {
+        if(i == 1 || i == 2)
+            printf("%d ", num1);
+        else {
+            soma = num1 + num2;
+            printf("%d ", soma);
+            num1 = num2;
+            num2 = soma;
+        }
+    }
+    
+    return 0;
+}

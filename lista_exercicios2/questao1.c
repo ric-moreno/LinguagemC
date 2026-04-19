3eebb988bc043f00476485dabf912c993ec0798e
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-19 13:46:45 -0400

    create-questao1.c

---
+++ lista_exercicios2/questao1.c
@@ -0,0 +1,15 @@
+/*1) Escreva um programa em linguagem C que imprima a seguinte mensagem na tela: 
+“Eu vou aprender a programar em Linguagem C na disciplina Algoritmos e Programação I”.*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main()
+
+{
+	setlocale(LC_ALL, "portuguese");
+	
+	printf("Eu vou aprender a programar em Linguagem C na disciplina Algoritmos e Programação I");
+	
+	return 0;
+}

e4c6a35a0e317d8d5cf2ec9e5b7575a3fb797032
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-19 14:34:40 -0400

    create questao1.c

---
+++ lista_exercicios5/questao1.c
@@ -0,0 +1,21 @@
+/*1) Escreva um programa que imprima a tabuada de multiplicação de um número digitado pelo usuário. 
+O programa deve pedir para o usuário digitar um número de 1 até 9 e imprimir a tabuada desse número.*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int num, i, resultado;
+	
+	printf("Digite um numero de 1 a 9: ");
+	scanf("%d", &num);
+	
+	for(i = 1; i <= 9; i++){
+		
+		resultado = num * i;
+		printf("%d x %d = %d\n", num, i, resultado);
+		
+	}
+	
+	return 0;
+}

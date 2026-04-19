c01c441a8e06a50cb11cc4a375f876ed437c655a
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-19 13:48:22 -0400

    Create questao2.c

---
+++ lista_exercicios2/questao2.c
@@ -0,0 +1,13 @@
+/*2) Altere o programa anterior para que a mensagem seja dividida e apresentada em três linhas diferentes.*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+  
+	setlocale(LC_ALL, "portuguese");
+	
+	printf("Eu vou aprender\na programar em Linguagem C\nna disciplina Algoritmos e Programação I\n");
+	
+	return 0;
+}

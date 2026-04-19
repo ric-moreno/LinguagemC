d694fcf72e4b1a9c46259d3eef9abeb656db4ee2
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-19 14:15:04 -0400

    Create questao4.c

---
+++ lista_exercicios4/questao4.c
@@ -0,0 +1,23 @@
+/*4) Escreva um programa que leia N números inteiros positivos e, 
+ao final, informe qual foi o maior número digitado. 
+O programa deve parar a leitura dos números quando o usuário digitar zero ou um número negativo.*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int num, maior = 0;
+	
+	while(num > 0){
+		
+		printf("Digite um numero inteiro positivo (0 ou negativo para interromper): ");
+		scanf("%d", &num);
+		
+		if (num > maior)
+			
+			maior =  num;
+	}
+	
+	printf("O maior numero digitado e: %d", maior);
+	return 0;
+}

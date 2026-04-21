47f04e43989d0bcd00bce2707dc1f60ca0a80ce9
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-21 12:43:02 -0400

    Update questao1.c

--- lista_exercicios5/questao1.c
+++ lista_exercicios5/questao1.c
@@ -1,6 +1,8 @@
 /*1) Escreva um programa que imprima a tabuada de multiplicação de um número digitado pelo usuário. 
 O programa deve pedir para o usuário digitar um número de 1 até 9 e imprimir a tabuada desse número.*/
 
+/*IMPRIME A TABUADA DE UM NUMERO ESPECIFICO*/
+
 #include<stdio.h>
 
 int main(){
@@ -18,4 +20,51 @@ int main(){
 	}
 	
 	return 0;
+}
+
+
+/*TODOS OS NUMEROS DE 1 A 9*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int n, i;
+	
+	printf("SOMA\n");
+	for(n = 1; n <= 10; n++){
+		
+		for(i = 1; i <= 10; i++){
+			printf("%d + %d = %d\n", n, i, n + i);
+		}
+	printf("\n");
+	}
+	
+	printf("SUBTRACAO\n");
+	for(n = 1; n <= 10; n++){
+		
+		for(i = 1; i <= 10; i++){
+			printf("%d - %d = %d\n", n, i, n - i);
+		}
+	printf("\n");
+	}
+	
+	printf("MULTIPLICACAO\n");
+	for(n = 1; n <= 10; n++){
+		
+		for(i = 1; i <= 10; i++){
+			printf("%d x %d = %d\n", n, i, n * i);
+		}
+	printf("\n");
+	}
+	
+	printf("DIVISAO\n");
+	for(n = 1; n <= 10; n++){
+		
+		for(i = 1; i <= 10; i++){
+			printf("%d / %d = %d\n", n, i, n / i);
+		}
+	printf("\n");
+	}
+	
 }

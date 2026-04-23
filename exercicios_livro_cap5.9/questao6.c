b0721b1c6e6609170cc43a0df159d134e0c19115
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-22 20:51:23 -0400

    Create questao6.c

---
+++ exercicios_livro_cap5.9/questao6.c
@@ -0,0 +1,18 @@
+/*6) Faça um programa que mostre uma contagem regressiva na tela, iniciando em 10 e terminando em 0. 
+Mostre uma mensagem “FIM!” após a contagem.*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int i;
+	
+	for(i = 10; i >= 0; i--){
+	
+		printf("%d\n", i);
+	}
+	
+	printf("FIM");
+	return 0;
+	
+}

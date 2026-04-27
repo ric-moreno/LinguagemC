a06cf0210d22b2dfe964308a6d8a47f45b4ccc53
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-27 11:31:18 -0400

    Create questao11.c

---
+++ exercicios_livro_cap4.7/questao11.c
@@ -0,0 +1,45 @@
+/*11) Faça um programa que informe o mês de acordo com o número digitado pelo usuário. Exemplo: Entrada = 4. Saída = Abril.*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "portuguese");
+	
+	int mes;
+	
+	printf("Digite o mês (1-12): ");
+	scanf("%d",&mes);
+	
+	switch(mes){
+		case 1: printf("Janeiro");
+			break;
+		case 2: printf("Fevereiro");
+			break;
+		case 3: printf("Março");
+			break;
+		case 4: printf("Abril");
+			break;
+		case 5: printf("Maio");
+			break;
+		case 6: printf("Junho");
+			break;
+		case 7: printf("Julho");
+			break;
+		case 8: printf("Agosto");
+			break;
+		case 9: printf("Setembro");
+			break;
+		case 10: printf("Outubro");
+			break;
+		case 11: printf("Novembro");
+			break;
+		case 12: printf("Dezembro");
+			break;
+		default:
+			printf("Inválido");
+	}
+	
+	return 0;
+}

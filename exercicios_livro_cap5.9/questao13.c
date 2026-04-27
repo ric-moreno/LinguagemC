fb0376866b1c065f4218e4cc3f1f414bdb784cb2
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-27 15:23:09 -0400

    Create questao13.c

---
+++ exercicios_livro_cap5.9/questao13.c
@@ -0,0 +1,13 @@
+/*13) Faça um programa que exiba a soma de todos os números naturais abaixo de 1.000 que são múltiplos de 3 ou 5.*/
+
+int main(){
+	
+	int i, soma = 0;
+	
+	for(i = 1; i < 1000; i++){
+		if(i % 3 == 0 || i % 5 == 0)
+		soma += i;
+	}
+	printf("A soma dos multiplos de 3 ou 5 eh %d", soma);
+	return 0;
+}

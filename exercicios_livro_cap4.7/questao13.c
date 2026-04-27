3dbd11ae7c40323f07caac005c6178ee139dd6ed
Pedro Ricardo Moreno <134879635+ric-moreno@users.noreply.github.com>
2026-04-27 11:27:51 -0400

    Create questao13.c

---
+++ exercicios_livro_cap4.7/questao13.c
@@ -0,0 +1,61 @@
+/*13) Faça um programa que mostre ao usuário um menu com quatro opções de operações matemáticas (as operações básicas, por exemplo). 
+O usuário escolhe uma das opções, e o seu programa pede dois valores numéricos e realiza a operação, mostrando o resultado.*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "portuguese");	
+	int num1, num2, resultado;
+	char op;
+	
+	printf("Digite a operação desejada + - * /: ");
+	op = getchar();
+	
+	printf("digite o primeiro número: ");
+	scanf("%d", &num1);
+	
+	printf("digite o segundo número: ");
+	scanf("%d", &num2);
+	
+	switch(op){
+		case '+':{
+			resultado = num1 + num2;
+			printf("Soma: %d", resultado);
+			break;
+		}
+			
+		case '-':{
+			resultado = num1 - num2;
+			printf("Subtração: %d", resultado);
+			break;
+		}
+		
+		case '*':{
+			resultado = num1 * num2;
+			printf("Multiplicação: %d", resultado);
+			break;
+		}
+		
+		case '/':{
+			
+			if (num1 == 0 || num2 == 0){
+				printf("Divisão por zero");
+				break;
+			}
+			
+			else{
+				resultado = num1 / num2;
+				printf("Divisão: %d", resultado);
+				break;
+			}
+		}
+		
+		default:
+			printf("Operação Inválida");
+	}
+	
+	return 0;
+
+}
